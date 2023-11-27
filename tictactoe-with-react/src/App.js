65b7fd8873e6dbd170bb4529497f3316b246a445
AtulRawat <workerhere12@gmail.com>
2023-11-28 00:22:35 +0530

    #

--- tictactoe-with-react/src/App.js
+++ tictactoe-with-react/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function Square({ value }) {
+
+  function handleClick(e) {
+    console.log(e.target.innerText);
+    console.log("clicked");
+    /*// let arr = [];
+    // for (let index = 0; index < arr.length; index++) {
+    //   arr[index].push(e.target);
+
+    //   // console.log(arr);
+    // }
+    // console.log(arr);*/
+  }
+  return <div className="square" onClick={handleClick}>{value}</div>
 }
 
-export default App;
+
+export default function Board() {
+  return (
+  <div className="game">
+    <div className="board-row">
+     <Square value={"X"} id="1" />
+     <Square value={"X"} id="2"/>
+     <Square value={"X"} id="3"/>
+    </div>
+    <div className="board-row">
+     <Square value={"X"} id="4"/>
+     <Square value={"X"} id="5"/>
+     <Square value={"X"} id="6"/>
+    </div>
+    <div className="board-row">
+     <Square value={"X"} id="7"/>
+     <Square value={"X"} id="8"/>
+     <Square value={"X"} id="9"/>
+    </div>
+  </div>);
+}
